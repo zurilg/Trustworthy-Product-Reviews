0b693655014635e77da245e4b8bde546ece01ffb
zurilg <zurilanegriffore@cmail.carleton.ca>
2025-10-25 17:17:19 -0400

    Base SpringBoot project with Maven.

---
+++ src/test/java/org/trustworthyreviews/TrustworthyProductReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.trustworthyreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustworthyProductReviewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
