ba8f90b6a620893fec67fb59a64e5c55fe2a1f73
wildildee <jacobwilde@cmail.carleton.ca>
2025-10-29 15:15:33 -0400

    Renamed to FrontController

--- src/main/java/org/trustworthyreviews/FrontController.java
+++ src/main/java/org/trustworthyreviews/FrontController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class FrontController {
 
     @GetMapping("/")
     public String home(Model model) {
