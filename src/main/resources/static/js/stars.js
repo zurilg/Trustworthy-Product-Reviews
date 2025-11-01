7fbacc82fb5b3a8bc2a9614951d34813918ea7bb
wildildee <jacobwilde@cmail.carleton.ca>
2025-11-01 13:12:46 -0400

    Stars now are dynamically rendered based on the data-stars field in the star-box div

---
+++ src/main/resources/static/js/stars.js
@@ -0,0 +1,14 @@
+$(() => {
+    $(".star-box").each((i, box) => {
+        box = $(box)
+        // Append 5 blank stars
+        for(let i = 0; i < 5; i ++) box.append($("<span class='star out'></span>"))
+        // Loop through those stars
+        box.children().each( (i, star) => {
+            // For each star in box check against the box rating value and change
+            if(box.data("stars") >= i+1)        star.className = "star full"
+            else if(box.data("stars") >= i+0.5) star.className = "star half"
+            else                                star.className = "star out"
+        })
+    })
+})
